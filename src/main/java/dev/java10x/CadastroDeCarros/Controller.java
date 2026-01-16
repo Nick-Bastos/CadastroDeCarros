0c390a65731ed1a86197d44204ecb308198575a2
Nickson Bastos <Nick.l.bastos11@gmail.com>
2026-01-16 17:45:57 -0300

    Criado a Controller

---
+++ src/main/java/dev/java10x/CadastroDeCarros/Controller.java
@@ -0,0 +1,14 @@
+package dev.java10x.CadastroDeCarros;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController //Define a classe como controller REST
+@RequestMapping //Mapeia URL + método HTTP
+public class Controller {
+    @GetMapping("/boasvindas")//Mapeia requisições HTTP GET
+    public String boasVindas(){
+        return "Essa é minha primeira mensagem nessa rota";
+    }
+}
